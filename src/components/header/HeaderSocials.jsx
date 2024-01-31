2eba8f8a5b764757c885d947b49edd4f338c27e9
Muhammad Herdy Iskandar <herdy2231120@itpln.ac.id>
2024-02-01 00:13:52 +0700

    Add files via upload

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub, FaInstagram } from 'react-icons/fa';
+import { FaAngellist } from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/dyzulk/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/dyzulk" target="_blank" rel="noreferrer" ><FaGithub /></a>
+      <a href="https://instagram.com/dyzulk" target="_blank" rel="noreferrer" ><FaInstagram /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
